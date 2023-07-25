b59101e7fd42b68d040dbeb1f7a2f8e889c525b9
Fatim-de <Fatimaeyad195@gmail.com>
2023-07-25 21:57:20 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
 int main()
+int main()
 {
+int sum;
+sum=intger-add(5,3);
+printf("the addtion of 5,3%d/n,sum");
+
 
     printf("Hello world");
 for (size_t i = 5; i < count; i++)
-{
-      printf("mohamed alfadel");
+
+      
+      
+
 
 }
 
